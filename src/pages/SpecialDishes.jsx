0d54ab945221b141783d4bf3892e552138f80cf4
rifathasan <mdmehedi1111bd@gmail.com>
2024-09-22 15:51:53 +0600

    added popular filter by catetory

--- src/pages/SpecialDishes.jsx
+++ src/pages/SpecialDishes.jsx
@@ -1,8 +1,103 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+
+
+
+
 
 const SpecialDishes = () => {
+
+const [recipes,setRecipes] =useState([])
+const slide = React.useRef(null)
+console.log(recipes)
+
+useEffect(() => {
+  fetch("/menu.json")
+    .then((res) => res.json())
+    .then((data) => {
+      const specials = data.filter((item) => item.category === "popular");
+      // console.log(specials)
+      setRecipes(specials);
+    });
+}, []);
+
+
+
+  const settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  };
+
+
+  
+
   return (
-    <div>SpecialDishes</div>
+    <div className='secetion-container my-20'>
+         <div className='text-left'>
+            <p className='subtitle'>Special Dishes</p>
+            <h2 className='title md:w-[520px]'>Standout Dishes Form Our Manu</h2> 
+        </div>
+
+        <Slider {...settings}>
+        <div>
+          <h3>1</h3>
+        </div>
+        <div>
+          <h3>2</h3>
+        </div>
+        <div>
+          <h3>3</h3>
+        </div>
+        <div>
+          <h3>4</h3>
+        </div>
+        <div>
+          <h3>5</h3>
+        </div>
+        <div>
+          <h3>6</h3>
+        </div>
+        <div>
+          <h3>7</h3>
+        </div>
+        <div>
+          <h3>8</h3>
+        </div>
+      </Slider>
+     
+     </div>
   )
 }
 
